994dcf08cb9c19cb4448c4a3f164c4b68798ad46
julian0808 <julianm08@gmail.com>
2024-03-03 14:34:49 -0500

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Julian Mesa on 3/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
